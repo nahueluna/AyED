acff4d55f11b009f6a6f08162d5d34162791d1f5
nahueluna <nahuelnicolasluna@hotmail.com>
2024-04-11 19:21:07 -0300

    Modificacion practica2.ejercicio9

--- practica2/ejercicio9/ParcialArboles.java
+++ practica2/ejercicio9/ParcialArboles.java
@@ -4,8 +4,8 @@ import practica2.ejercicio1y2.BinaryTree;
 
 public class ParcialArboles {
 	
-	public BinaryTree<int[]> sumAndDif(BinaryTree<Integer> arbol) {
-		BinaryTree<int[]> newTree = new BinaryTree<int[]>();
+	public BinaryTree<SumAndDif> sumAndDif(BinaryTree<Integer> arbol) {
+		BinaryTree<SumAndDif> newTree = new BinaryTree<SumAndDif>();
 		
 		if(!arbol.isEmpty())
 			newTree = procesarArbolSumAndDif(arbol, 0, 0);
@@ -13,8 +13,8 @@ public class ParcialArboles {
 		return newTree;
 	}
 	
-	private BinaryTree<int[]> procesarArbolSumAndDif(BinaryTree<Integer> arbol, int datoSuma, int datoPadre) {
-		BinaryTree<int[]> newTree = new BinaryTree<int[]>();
+	private BinaryTree<SumAndDif> procesarArbolSumAndDif(BinaryTree<Integer> arbol, int datoSuma, int datoPadre) {
+		BinaryTree<SumAndDif> newTree = new BinaryTree<SumAndDif>();
 		
 		if(arbol.hasLeftChild())
 			newTree.addLeftChild(procesarArbolSumAndDif(arbol.getLeftChild(), (datoSuma + arbol.getData()), arbol.getData()));
@@ -22,9 +22,7 @@ public class ParcialArboles {
 		if(arbol.hasRightChild())
 			newTree.addRightChild(procesarArbolSumAndDif(arbol.getRightChild(), (datoSuma + arbol.getData()), arbol.getData()));
 		
-		int[] data = new int[2];
-		data[0] = arbol.getData() + datoSuma;
-		data[1] = arbol.getData() - datoPadre;
+		SumAndDif data = new SumAndDif((arbol.getData() + datoSuma), (arbol.getData() - datoPadre));
 		
 		newTree.setData(data);
 		
---
+++ practica2/ejercicio9/SumAndDif.java
@@ -0,0 +1,31 @@
+package practica2.ejercicio9;
+
+public class SumAndDif {
+	private int suma;
+	private int diferencia;
+	
+	public SumAndDif() {
+		
+	}
+	
+	public SumAndDif(int sum, int dif) {
+		this.suma = sum;
+		this.diferencia = dif;
+	}
+
+	public int getSuma() {
+		return suma;
+	}
+
+	public void setSuma(int suma) {
+		this.suma = suma;
+	}
+
+	public int getDiferencia() {
+		return diferencia;
+	}
+
+	public void setDiferencia(int diferencia) {
+		this.diferencia = diferencia;
+	}
+}
