5d047c248a657d74fa2fc6e9c66a5a7439ddb221
nahueluna <nahuelnicolasluna@hotmail.com>
2024-04-24 21:05:49 -0300

    Se agrega p3, ej7 y 8

---
+++ practica3/ejercicio7/Caminos.java
@@ -0,0 +1,48 @@
+package practica3.ejercicio7;
+
+import java.util.LinkedList;
+import java.util.List;
+import practica3.ejercicio01_03_05.GeneralTree;
+
+public class Caminos {
+	private GeneralTree<Integer> generalTree = new GeneralTree<>();
+	
+	public Caminos() {
+		
+	}
+	
+	public Caminos(GeneralTree<Integer> generalTree) {
+		this.generalTree = generalTree;
+	}
+	
+	public GeneralTree<Integer> getGeneralTree() {
+		return generalTree;
+	}
+
+	public List<Integer> caminoAHojaMasLejana() {
+		List<Integer> camino = new LinkedList<Integer>();
+		List<Integer> lista = new LinkedList<Integer>();
+		
+		if(!getGeneralTree().isEmpty())
+			procesarCamino(getGeneralTree(), lista, camino);
+		
+		return camino;
+	}
+	
+	private void procesarCamino(GeneralTree<Integer> tree, List<Integer> lista, List<Integer> camino) {
+		lista.add(tree.getData());
+		
+		if(!tree.isLeaf()) {
+			List<GeneralTree<Integer>> children = tree.getChildren();
+			for(GeneralTree<Integer> child: children)
+				procesarCamino(child, lista, camino);
+		}
+		else if(lista.size() > camino.size()) {
+			camino.removeAll(camino);
+			camino.addAll(lista);
+		}
+		
+		lista.remove(lista.size() - 1);
+		
+	}
+}
---
+++ practica3/ejercicio8/Navidad.java
@@ -0,0 +1,40 @@
+package practica3.ejercicio8;
+
+import java.util.List;
+
+import practica3.ejercicio01_03_05.GeneralTree;
+
+public class Navidad<T> {
+	private GeneralTree<T> abeto = new GeneralTree<>();
+
+	public Navidad(GeneralTree<T> abeto) {
+		this.abeto = abeto;
+	}
+	
+	public String esAbetoNavidenio() {
+		boolean esAbeto = false;
+		if(!abeto.isEmpty()) esAbeto = procesarAbeto(abeto);
+		
+		// if esAbeto = true return "Yes". Else return "No"
+		return esAbeto ? "Yes" : "No";
+		
+	}
+	
+	private boolean procesarAbeto(GeneralTree<T> gt) {
+		int cantHojas = 0;
+		boolean esAbeto = true;
+		
+		List<GeneralTree<T>> children = gt.getChildren();
+			
+		if(children.size() >= 3) {
+			for(GeneralTree<T> child: children) {
+				if(child.isLeaf()) cantHojas++;
+				else esAbeto = procesarAbeto(child);
+			}
+				
+			esAbeto = esAbeto && cantHojas >= 3;
+		}
+		
+		return esAbeto;
+	}
+}
